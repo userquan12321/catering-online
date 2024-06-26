402471820c25a7b0bfc5b257d2533ec9eb8a9c44
userquan12321 <pmquan248@gmail.com>
2024-06-26 17:42:54 +0700

    Update Search API

---
+++ backend/Models/CuisineType.cs
@@ -0,0 +1,24 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace backend.Models
+{
+    public class CuisineType
+    {
+        [Key]
+        public int ID { get; set; }
+        [Required]
+        [StringLength(255, MinimumLength = 2)]
+        public string CuisineName { get; set; }
+        public DateTime CreatedAt { get; set; }
+        public DateTime UpdatedAt { get; set; }
+        public ICollection<Item> Items { get; set; }
+        public CuisineType()
+        {
+            ID = 0;
+            CuisineName = "";
+            CreatedAt = DateTime.UtcNow;
+            UpdatedAt = DateTime.UtcNow;
+            Items = [];
+        }
+    }
+}
---
+++ backend/Models/Item.cs
@@ -0,0 +1,43 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace backend.Models
+{
+    public class Item
+    {
+        [Key]
+        public int ID { get; set; }
+        [Required]
+        public string Name { get; set; }
+        [Required]
+        public string Image { get; set; }
+        [Required]
+        public int ServesCount { get; set; }
+        [Required]
+        public decimal Price { get; set; }
+        [Required]
+        public int CatererID { get; set; }
+        [ForeignKey("CatererID")]
+        public Caterer? Caterer { get; set; }
+        [Required]
+        public int CuisineID { get; set; }
+        [ForeignKey("CuisineID")]
+        public CuisineType? CuisineType { get; set; }
+        public DateTime CreatedAt { get; set; }
+        public DateTime UpdatedAt { get; set; }
+        public Item()
+        {
+            ID = 0;
+            Name = "";
+            Image = "";
+            ServesCount = 0;
+            Price = 0M;
+            CatererID = 0;
+            Caterer = null;
+            CuisineID = 0;
+            CuisineType = null;
+            CreatedAt = DateTime.UtcNow;
+            UpdatedAt = DateTime.UtcNow;
+        }
+    }
+}
