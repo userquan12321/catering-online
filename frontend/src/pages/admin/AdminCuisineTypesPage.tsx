2b8bf2c3d38f1835d1f4998d7028ff2ff4f9e879
ntuanthanh <ntuanthanh184@gmail.com>
2024-07-21 03:17:36 +0700

    Refactor cuisine type

--- frontend/src/pages/admin/AdminCuisineTypesPage.tsx
+++ frontend/src/pages/admin/AdminCuisineTypesPage.tsx
@@ -1,15 +1,20 @@
-import { useState, useEffect } from 'react';
-import { Controller, useForm } from 'react-hook-form';
-import { yupResolver } from '@hookform/resolvers/yup';
-import type { TableColumnsType } from 'antd';
-import { Button, Col, Form, Input, message, Modal, Row, Typography } from 'antd';
+import { useState } from 'react'
+import { Controller, useForm } from 'react-hook-form'
+import { yupResolver } from '@hookform/resolvers/yup'
+import type { TableColumnsType } from 'antd'
+import { Button, Col, Form, Input, message, Modal, Row, Typography } from 'antd'
 
-import { useAddCuisineMutation, useEditCuisineMutation, useDeleteCuisineMutation, useGetCuisinesQuery } from '@/apis/admin.api';
-import CustomTable from '@/components/common/CustomTable';
-import UploadWidget from '@/components/common/UploadWidget';
-import { useAlert } from '@/hooks/globals/useAlert.hook';
-import { CuisineInput, CuisineType } from '@/types/cuisine.type';
-import { cuisineTypeValidation } from '@/validations/cuisine-type.validation';
+import {
+  useAddCuisineMutation,
+  useDeleteCuisineMutation,
+  useEditCuisineMutation,
+  useGetCuisinesQuery,
+} from '@/apis/admin.api'
+import CustomTable from '@/components/common/CustomTable'
+import UploadWidget from '@/components/common/UploadWidget'
+import { useAlert } from '@/hooks/globals/useAlert.hook'
+import { CuisineInput, CuisineType } from '@/types/cuisine.type'
+import { cuisineTypeValidation } from '@/validations/cuisine-type.validation'
 
 const AdminCuisineTypesPage = () => {
   const {
@@ -20,56 +25,39 @@ const AdminCuisineTypesPage = () => {
     setValue,
   } = useForm({
     resolver: yupResolver(cuisineTypeValidation),
-  });
+  })
 
-  const { handleAlert, contextHolder } = useAlert();
-  const { data: cuisines = [], isLoading: isLoadingData } = useGetCuisinesQuery();
+  const { handleAlert, contextHolder } = useAlert()
+  const { data: cuisines = [], isLoading: isLoadingData } =
+    useGetCuisinesQuery()
 
-  const [addCuisine, { isLoading: isAdding }] = useAddCuisineMutation();
-  const [editCuisine, { isLoading: isEditing }] = useEditCuisineMutation();
-  const [deleteCuisine] = useDeleteCuisineMutation();
+  const [addCuisine, { isLoading: addLoading }] = useAddCuisineMutation()
+  const [editCuisine, { isLoading: editLoading }] = useEditCuisineMutation()
+  const [deleteCuisine] = useDeleteCuisineMutation()
 
-  const [openAddDrawer, setOpenAddDrawer] = useState(false);
-  const [openEditDrawer, setOpenEditDrawer] = useState(false);
-  const [currentCuisineId, setCurrentCuisineId] = useState<number | null>(null);
-
-  useEffect(() => {
-    if (openAddDrawer) {
-      reset();
-    }
-  }, [openAddDrawer, reset]);
-
-  useEffect(() => {
-    if (openEditDrawer) {
-      const cuisineToEdit = cuisines.find(cuisine => cuisine.id === currentCuisineId);
-      if (cuisineToEdit) {
-        setValue('cuisineName', cuisineToEdit.cuisineName);
-        setValue('description', cuisineToEdit.description);
-        setValue('cuisineImage', cuisineToEdit.cuisineImage);
-      }
-    }
-  }, [currentCuisineId, cuisines, setValue, openEditDrawer]);
+  const [openDrawer, setOpenDrawer] = useState(false)
+  const [currentCuisineId, setCurrentCuisineId] = useState<number | null>(null)
 
   const onSubmit = async (values: CuisineInput) => {
     try {
-      if (currentCuisineId !== null) {
-        const res = await editCuisine({ id: currentCuisineId, ...values });
+      if (currentCuisineId) {
+        const res = await editCuisine({ id: currentCuisineId, ...values })
         handleAlert(res, () => {
-          setOpenEditDrawer(false);
-          reset();
-          setCurrentCuisineId(null);
-        });
-      } else {
-        const res = await addCuisine(values);
-        handleAlert(res, () => {
-          setOpenAddDrawer(false);
-          reset();
-        });
+          setOpenDrawer(false)
+          setCurrentCuisineId(null)
+          reset()
+        })
+        return
       }
+      const res = await addCuisine(values)
+      handleAlert(res, () => {
+        setOpenDrawer(false)
+        reset()
+      })
     } catch (error) {
-      message.error('Failed to add cuisine');
+      message.error('Failed to add cuisine')
     }
-  };
+  }
 
   const columns: TableColumnsType<CuisineType> = [
     {
@@ -89,34 +77,51 @@ const AdminCuisineTypesPage = () => {
       title: 'Description',
       dataIndex: 'description',
     },
-  ];
+  ]
+
+  const handleClose = () => {
+    setOpenDrawer(false)
+    setCurrentCuisineId(null)
+  }
+
+  const handleAdd = () => {
+    setOpenDrawer(true)
+    reset()
+  }
 
   const handleEdit = (id: number) => {
-    setCurrentCuisineId(id);
-    setOpenEditDrawer(true);
-  };
+    setOpenDrawer(true)
+    setCurrentCuisineId(id)
+
+    const cuisineToEdit = cuisines.find((cuisine) => cuisine.id === id)
+
+    if (cuisineToEdit) {
+      setValue('cuisineName', cuisineToEdit.cuisineName)
+      setValue('description', cuisineToEdit.description)
+      setValue('cuisineImage', cuisineToEdit.cuisineImage)
+    }
+  }
 
   const handleDelete = (id: number) => {
-    const cuisineToDelete = cuisines.find(cuisine => cuisine.id === id);
+    const cuisineToDelete = cuisines.find((cuisine) => cuisine.id === id)
     if (!cuisineToDelete) {
-      message.error('Cuisine not found');
-      return;
+      message.error('Cuisine not found!')
+      return
     }
     Modal.confirm({
       title: `Are you sure you want to delete "${cuisineToDelete.cuisineName}"?`,
       onOk: async () => {
         try {
-
-          await deleteCuisine(id);
-          message.success('Cuisine deleted successfully');
+          const res = await deleteCuisine(id)
+          handleAlert(res)
         } catch (error) {
-          message.error('Failed to delete cuisine');
+          message.error('Failed to delete cuisine!')
         }
       },
-    });
-  };
+    })
+  }
 
-  const renderAddDrawerContent = () => (
+  const renderDrawerContent = () => (
     <Form layout="vertical" onFinish={handleSubmit(onSubmit)}>
       <Form.Item
         label="Cuisine name"
@@ -169,99 +174,39 @@ const AdminCuisineTypesPage = () => {
           </Button>
         </Col>
         <Col>
-          <Button type="primary" htmlType="submit" disabled={isAdding || isEditing}>
-            Add
+          <Button
+            type="primary"
+            htmlType="submit"
+            disabled={addLoading || editLoading}
+          >
+            {currentCuisineId ? 'Edit' : 'Add'}
           </Button>
         </Col>
       </Row>
 
       <>{contextHolder}</>
     </Form>
-  );
-
-  const renderEditDrawerContent = () => (
-    <Form layout="vertical" onFinish={handleSubmit(onSubmit)}>
-      <Form.Item
-        label="Cuisine name"
-        required
-        help={errors.cuisineName?.message}
-        validateStatus={errors.cuisineName ? 'error' : ''}
-      >
-        <Controller
-          name="cuisineName"
-          control={control}
-          defaultValue=""
-          render={({ field }) => <Input {...field} />}
-        />
-      </Form.Item>
-
-      <Form.Item
-        label="Description"
-        required
-        help={errors.description?.message}
-        validateStatus={errors.description ? 'error' : ''}
-      >
-        <Controller
-          name="description"
-          control={control}
-          defaultValue=""
-          render={({ field }) => <Input.TextArea rows={3} {...field} />}
-        />
-      </Form.Item>
-
-      <Form.Item label="Cuisine image" required>
-        <Controller
-          name="cuisineImage"
-          control={control}
-          defaultValue=""
-          render={({ field }) => (
-            <div className="upload-image">
-              {field.value ? (
-                <img className="w-full" src={field.value} alt="Cuisine Image" />
-              ) : null}
-              <UploadWidget onChange={field.onChange} />
-            </div>
-          )}
-        />
-      </Form.Item>
-
-      <Row justify="end" gutter={8}>
-        <Col>
-          <Button type="default" htmlType="reset" onClick={() => reset()}>
-            Reset
-          </Button>
-        </Col>
-        <Col>
-          <Button type="primary" htmlType="submit" disabled={isAdding || isEditing}>
-            Edit
-          </Button>
-        </Col>
-      </Row>
-
-      <>{contextHolder}</>
-    </Form>
-  );
+  )
 
   if (isLoadingData) {
-    return <p>Loading...</p>;
+    return <p>Loading...</p>
   }
 
   return (
     <CustomTable
-      openDrawer={openAddDrawer}
-      setOpenDrawer={setOpenAddDrawer}
-      addText="Add Cuisine"
-      renderAddDrawerContent={renderAddDrawerContent}
-      renderEditDrawerContent={renderEditDrawerContent}
+      openDrawer={openDrawer}
+      onAdd={handleAdd}
+      onClose={handleClose}
+      name="Cuisine Type"
+      renderDrawerContent={renderDrawerContent}
       customColumns={columns}
       onDelete={handleDelete}
       onEdit={handleEdit}
+      isEditing={!!currentCuisineId}
       dataSource={cuisines}
       rowKey={(record) => record.id}
-      openEditDrawer={openEditDrawer}
-      setOpenEditDrawer={setOpenEditDrawer}
     />
-  );
-};
+  )
+}
 
-export default AdminCuisineTypesPage;
+export default AdminCuisineTypesPage
