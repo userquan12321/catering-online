8b3517c7d48b40112563e1b3729e96c79b9860c9
ntuanthanh <ntuanthanh184@gmail.com>
2024-07-25 19:55:13 +0700

    Fix UI messages

--- frontend/src/components/Messages/MessageDetail.tsx
+++ frontend/src/components/Messages/MessageDetail.tsx
@@ -1,6 +1,6 @@
-import { useState } from 'react'
+import { useRef, useState } from 'react'
 import { SendOutlined } from '@ant-design/icons'
-import { Avatar, Button, Empty, Input, Space } from 'antd'
+import { Avatar, Button, Empty, Input, InputRef, Space } from 'antd'
 
 import { useGetMessagesQuery, useSendMessageMutation } from '@/apis/message.api'
 
@@ -8,6 +8,7 @@ interface MessageDetailProps {
   receiverId: any // replace 'any' with the actual type of the menu item
 }
 const MessageDetail = ({ receiverId }: MessageDetailProps) => {
+  const inputRef = useRef<InputRef>(null)
   const { data, isLoading } = useGetMessagesQuery(+receiverId, {
     skip: !receiverId,
   })
@@ -15,7 +16,6 @@ const MessageDetail = ({ receiverId }: MessageDetailProps) => {
   const [inputMessage, setInputMessage] = useState('')
 
   if (isLoading || isSendLoading) return <div>Loading...</div>
-  // console.log('Message', data)
 
   if (!data) {
     return (
@@ -32,94 +32,87 @@ const MessageDetail = ({ receiverId }: MessageDetailProps) => {
 
   return (
     <div>
-      {data !== null && (
-        <div>
-          <div
-            style={{
-              display: 'flex',
-              alignItems: 'center',
-              gap: '5px',
-              padding: '10px',
-            }}
-          >
-            <Avatar src={<img src={data.receiver.image} alt="avatar" />} />
-            <p>{data.receiver.firstName + ' ' + data.receiver.lastName}</p>
-          </div>
-          <div
-            style={{
-              padding: '24px',
-              borderTop: '1px solid rgba(5, 5, 5, 0.06)',
-              overflow: 'y-scroll',
-              height: '300px',
-              maxHeight: '300px',
-              display: 'flex',
-              flexDirection: 'column',
-              justifyContent: 'flex-end',
-            }}
-          >
-            {[...data.message].reverse().map((message: any) =>
-              message.isSender ? (
-                <div
-                  style={{
-                    display: 'flex',
-                    gap: '5px',
-                    flexDirection: 'row-reverse',
-                  }}
-                  key={message.id}
-                >
-                  <p
-                    style={{
-                      padding: '8px',
-                      borderRadius: '8px 0 0 8px',
-                      border: '1px solid rgba(5, 5, 5, 0.06)',
-                    }}
-                  >
-                    {message.content}
-                  </p>
-                </div>
-              ) : (
-                <div
-                  style={{
-                    display: 'flex',
-                    gap: '5px',
-                  }}
-                  key={message.id}
-                >
-                  <Avatar
-                    src={<img src={data.receiver.image} alt="avatar" />}
-                  />
-                  <p
-                    style={{
-                      padding: '8px',
-                      borderRadius: '8px 0 0 8px',
-                      border: '1px solid rgba(5, 5, 5, 0.06)',
-                    }}
-                  >
-                    {message.content}
-                  </p>
-                </div>
-              ),
-            )}
-          </div>
-          <Space.Compact style={{ width: '100%' }}>
-            <Input
-              value={inputMessage}
-              onChange={(e) => setInputMessage(e.target.value)}
-              onKeyDown={(e) => {
-                if (e.key === 'Enter') {
-                  handleMessage()
-                  e.preventDefault()
-                }
+      <div
+        style={{
+          display: 'flex',
+          alignItems: 'center',
+          gap: '5px',
+          padding: '10px',
+        }}
+      >
+        <Avatar src={<img src={data.receiver.image} alt="avatar" />} />
+        <p>{data.receiver.firstName + ' ' + data.receiver.lastName}</p>
+      </div>
+      <div
+        style={{
+          padding: '24px',
+          borderTop: '1px solid rgba(5, 5, 5, 0.06)',
+          overflowY: 'scroll',
+          height: '300px',
+          maxHeight: '300px',
+        }}
+      >
+        {data.messages.map((message: any) =>
+          message.isSender ? (
+            <div
+              style={{
+                display: 'flex',
+                gap: '5px',
+                flexDirection: 'row-reverse',
               }}
-            />
-            <Button
-              type="primary"
-              onClick={handleMessage}
-              icon={<SendOutlined />}
-            ></Button>
-          </Space.Compact>
-        </div>
-      )}
+              key={message.id}
+            >
+              <p
+                style={{
+                  padding: '8px',
+                  borderRadius: '8px 0 0 8px',
+                  border: '1px solid rgba(5, 5, 5, 0.06)',
+                }}
+              >
+                {message.content}
+              </p>
+            </div>
+          ) : (
+            <div
+              style={{
+                display: 'flex',
+                gap: '5px',
+              }}
+              key={message.id}
+            >
+              <Avatar src={<img src={data.receiver.image} alt="avatar" />} />
+              <p
+                style={{
+                  padding: '8px',
+                  borderRadius: '8px 0 0 8px',
+                  border: '1px solid rgba(5, 5, 5, 0.06)',
+                }}
+              >
+                {message.content}
+              </p>
+            </div>
+          ),
+        )}
+      </div>
+      <Space.Compact style={{ width: '100%' }}>
+        <Input
+          ref={inputRef}
+          value={inputMessage}
+          onChange={(e) => setInputMessage(e.target.value)}
+          onKeyDown={(e) => {
+            if (e.key === 'Enter') {
+              handleMessage()
+              e.preventDefault()
+              inputRef.current?.focus()
+            }
+          }}
+        />
+        <Button
+          type="primary"
+          onClick={handleMessage}
+          icon={<SendOutlined />}
+        />
+      </Space.Compact>
     </div>
   )
 }
